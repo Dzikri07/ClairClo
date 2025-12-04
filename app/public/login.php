781293181236a36338e868bee2d609703e75670f
Dzikri07 <125238574+Dzikri07@users.noreply.github.com>
2025-12-04 11:53:32 +0700

    haops

--- app/public/login.php
+++ app/public/login.php
@@ -1,8 +1,6 @@
 <?php
 session_start();
 require_once __DIR__ . '/connection.php';
-    var_dump(getenv('MYSQLHOST'));
-    var_dump(getDB());
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $username = $_POST['user'] ?? '';
