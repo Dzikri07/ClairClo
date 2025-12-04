2e65ede33d1661716fbad3dd5de8e0d8be095cf6
Dzikri07 <125238574+Dzikri07@users.noreply.github.com>
2025-12-04 10:39:36 +0700

    Refactor environment variable retrieval to use getenvenv

--- app/public/connection.php
+++ app/public/connection.php
@@ -19,11 +19,12 @@ class DatabaseConnection
     private function __construct()
     {
         // Ambil variabel dari Railway
-        $this->host = $_ENV['MYSQLHOST'] ?? '127.0.0.1';
-        $this->db   = $_ENV['MYSQLDATABASE'] ?? 'clariocloud';
-        $this->user = $_ENV['MYSQLUSER'] ?? 'root';
-        $this->pass = $_ENV['MYSQLPASSWORD'] ?? '';
-        $this->port = $_ENV['MYSQLPORT'] ?? 3306;
+        $this->host = getenv('MYSQLHOST') ?: '127.0.0.1';
+        $this->db   = getenv('MYSQLDATABASE') ?: 'clariocloud';
+        $this->user = getenv('MYSQLUSER') ?: 'root';
+        $this->pass = getenv('MYSQLPASSWORD') ?: '';
+        $this->port = getenv('MYSQLPORT') ?: 3306;
+
 
         try {
             // DSN khusus Railway (harus pakai port)
