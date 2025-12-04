ff8dbb5dc3e38d9c2e6873f4991e956b089669c9
Dzikri07 <125238574+Dzikri07@users.noreply.github.com>
2025-12-04 13:26:21 +0700

    Update login.php

--- app/public/login.php
+++ app/public/login.php
@@ -1,6 +1,11 @@
 <?php
 session_start();
 require_once __DIR__ . '/connection.php';
+error_log("ENV CHECK: HOST=" . getenv('MYSQLHOST'));
+error_log("ENV CHECK: DB=" . getenv('MYSQLDATABASE'));
+error_log("ENV CHECK: USER=" . getenv('MYSQLUSER'));
+error_log("ENV CHECK: PASS=" . substr(getenv('MYSQLPASSWORD'), 0, 4) . "****");
+error_log("ENV CHECK: PORT=" . getenv('MYSQLPORT'));
 
 try {
     $test = getDB();
