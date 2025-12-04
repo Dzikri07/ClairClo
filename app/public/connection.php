4b0dc09657872821610690f8982117740778215f
Dzikri07 <125238574+Dzikri07@users.noreply.github.com>
2025-12-04 12:07:11 +0700

    Update connection.php

--- app/public/connection.php
+++ app/public/connection.php
@@ -3,6 +3,10 @@
  * connection.php - Database connection handler for Railway
  */
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 class DatabaseConnection
 {
     private static $instance = null;
